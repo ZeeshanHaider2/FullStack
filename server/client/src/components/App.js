e07eee8f86545c9ba98eca5a86f113863c8ae44c
Syed Zeeshan Haider <zeeshanhyf@gmail.com>
2019-12-11 02:54:37 +0100

    Browswer router + Route imported

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,8 +1,15 @@
-//This will hold our React Router stuff.
+//This will hold our React Router stuff/things for viewing
 import React from "react";
+import { BrowserRouter } from "react-router-dom"; //contains set of react router helpers for dom navigation
 //We have created a functional component that returns a little bit of JSX
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNe</h2>;
+const Landing = () => <h2>Landing</h2>;
+
 const App = () => {
-  return <div>Hi There!</div>;
+  return <div></div>;
 };
 
 export default App;
