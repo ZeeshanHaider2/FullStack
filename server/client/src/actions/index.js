bd34f9bffa2d7a688a34486af558c07caf7eecc5
Syed Zeeshan Haider <zeeshanhyf@gmail.com>
2019-12-12 12:43:51 +0100

    refactoring of promises to async/await

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -2,12 +2,18 @@
 import axios from "axios";
 import { FETCH_USER } from "./types";
 
-export const fetchUser = () => {
-  return function(dispatch) {
-    axios
-      .get("/api/current_user")
-      .then(res => dispatch({ type: FETCH_USER, payload: res }));
-  };
+//Refactoring to async/await 86
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get("/api/current_user");
+  dispatch({ type: FETCH_USER, payload: res });
 };
+
+// export const fetchUser = () => {
+//   return function(dispatch) {
+//     axios
+//       .get("/api/current_user")
+//       .then(res => dispatch({ type: FETCH_USER, payload: res }));
+//   };
+// };
 //fetchUser action creator, which is intended to make a request to our back-end api and somehow
 //communicate to our Oauth reducer whether Or Not our user is logged in. 83
